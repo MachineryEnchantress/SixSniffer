e1befdee045d11df67a7221cde69147446e43d5b
Naomi Wu <sexycyborg@protonmail.ch>
2022-01-14 09:40:48 +0800

    Add files via upload

---
+++ T-Watch2020-BluetoothScan/src/gui.h
@@ -0,0 +1,44 @@
+/*
+Copyright (c) 2019 lewis he
+This is just a demonstration. Most of the functions are not implemented.
+The main implementation is low-power standby.
+The off-screen standby (not deep sleep) current is about 4mA.
+Select standard motherboard and standard backplane for testing.
+Created by Lewis he on October 10, 2019.
+*/
+
+#ifndef __GUI_H
+#define __GUI_H
+
+typedef enum
+{
+    LV_ICON_BAT_EMPTY,
+    LV_ICON_BAT_1,
+    LV_ICON_BAT_2,
+    LV_ICON_BAT_3,
+    LV_ICON_BAT_FULL,
+    LV_ICON_CHARGE,
+    LV_ICON_CALCULATION
+} lv_icon_battery_t;
+
+typedef enum
+{
+    LV_STATUS_BAR_BATTERY_LEVEL = 0,
+    LV_STATUS_BAR_BATTERY_ICON = 1,
+    LV_STATUS_BAR_WIFI = 2,
+    LV_STATUS_BAR_BLUETOOTH = 3,
+} lv_icon_status_bar_t;
+
+#define RESULTS_PATH "/bledata/results.txt"
+#define CACHE_PATH "/bledata/cache.txt"
+#define GOLIST_PATH "/bledata/golist.txt"
+
+void setupGui();
+void updateStepCounter(uint32_t counter);
+void updateBatteryIcon(lv_icon_battery_t index);
+void wifi_list_add(const char *ssid);
+void wifi_connect_status(bool result);
+void updateBatteryLevel();
+
+void ble_list_add(char *str);
+#endif /*__GUI_H */
